de8b93e9409916639e184e55fd65312e12215721
Ankit Kumar Singh <63660669+WhiteDevil8821@users.noreply.github.com>
2024-02-05 06:51:03 +0000

    Stack and Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,3 +32,28 @@ let myObj = {
 const myFunction = function() {
     console.log("Hello World");
 }
+
+
+// +++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (non-Primitive)
+
+let myYTName = "Devil"
+
+let anotherName = myYTName
+anotherName = "WhiteDevil"
+
+console.log(myYTName);
+console.log(anotherName);
+
+let user1 = {
+    email : "user@abc.com",
+    upi : "user@ybl",
+}
+
+let user2 = user1
+
+user2.email = "ankit@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
